201e3406a33b6eab5b33d71e87e19c80f1917467
gbss2 <jwojwo@gmail.com>
2016-08-26 21:47:31 -0300

    Add files via upload

---
+++ src/massa/enrichment/ContingencyTable2x2.java
@@ -0,0 +1,9 @@
+package massa.enrichment;
+
+public class ContingencyTable2x2 {
+
+	public ContingencyTable2x2(Integer integer, int i, Integer integer2, int j) {
+
+	}
+
+}
---
+++ src/massa/enrichment/FishersExactTest.java
@@ -0,0 +1,16 @@
+package massa.enrichment;
+
+public class FishersExactTest {
+
+	private ContingencyTable2x2 table;
+
+	public FishersExactTest(ContingencyTable2x2 table) {
+		this.table = table;
+	}
+
+	public String getOneTailedSP() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
